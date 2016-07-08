50246e22e1886a445d19ef140af70f89bc9c6236
Jack Chen <yuxi.chen@berkeley.edu>
2016-07-08 12:14:27 -0700

    added regexPassword

---
+++ regexPassword.swift
@@ -0,0 +1,19 @@
+
+func checkTextSufficientComplexity(var text : String) -> Bool{
+    let capitalLetterRegEx  = ".*[A-Z]+.*"
+    var texttest = NSPredicate(format:"SELF MATCHES %@", capitalLetterRegEx)
+    var capitalresult = texttest.evaluateWithObject(text)
+    
+    let numberRegEx  = ".*[0-9]+.*"
+    var texttest1 = NSPredicate(format:"SELF MATCHES %@", numberRegEx)
+    var numberresult = texttest1.evaluateWithObject(text)
+    
+    let specialCharacterRegEx  = ".*[!&^%$#@()/]+.*"
+    var texttest2 = NSPredicate(format:"SELF MATCHES %@", specialCharacterRegEx)
+    var specialresult = texttest2.evaluateWithObject(text)
+    
+    return capitalresult && numberresult && specialresult && text.characters.count >= 8
+    
+}
+
+checkTextSufficientComplexity("Avatar123!") 
